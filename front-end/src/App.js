104d9b17981fa60a2f5f295728fd579004188e75
Avi Herman <aviherman5@gmail.com>
2025-10-27 10:15:54 -0400

    Add initial React front-end setup with minimal starter app

---
+++ front-end/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>NourishPet</h1>
+        <p>Transform nutrition tracking into an engaging, game-like experience</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
